4752b49a462c3a1c2fc5c6eab268a9c6422cb2c5
riadshalahinleon <riadshalahinleon@gmail.com>
2022-10-03 15:44:37 +0600

    loaded data with AXIOS AND REACHARTS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './components/Pricing/Pricing';
 import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <NavBar></NavBar>
         <Pricing></Pricing>
         <AssignmentMarks></AssignmentMarks>
+        <PhoneBar></PhoneBar>
     </div>
   );
 }
